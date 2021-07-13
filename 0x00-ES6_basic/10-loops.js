668327841a913557144f451d5d0ff0171a46a92d
Adam Taylor <tayloradam1999@gmail.com>
2021-07-13 00:40:03 +0000

    Pushing file for task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+  for (const idx of array) {
+    const value = appendString + idx;
+    arr.push(value);
+  }
+
+  return array;
+}
