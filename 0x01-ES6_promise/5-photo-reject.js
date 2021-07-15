32e6d64ec1279c6546b734f40053bb3cec052463
Adam Taylor <tayloradam1999@gmail.com>
2021-07-15 22:26:20 +0000

    reformatted logic for reject error

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,5 @@
 export default function uploadPhoto(fileName) {
-  return new Promise((reject) => reject(new Error(`${fileName} cannot be processed`)));
+  return new Promise((reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
+  });
 }
