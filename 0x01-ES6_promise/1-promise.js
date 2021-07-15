dbd0fd31732e520a7d67858e81c27d5e24c329cf
Adam Taylor <tayloradam1999@gmail.com>
2021-07-15 21:15:53 +0000

    Changed if conditional and added new Error in reject call

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
+    if (success) {
       const myObj = { status: 200, body: 'Success' };
       resolve(myObj);
     } else {
-        const errorObj = "The fake API is not working currently";
-        reject(errorObj);
+        const errorObj = 'The fake API is not working currently';
+        reject(new Error(errorObj));
     }
-  })
-};
+  });
+}
