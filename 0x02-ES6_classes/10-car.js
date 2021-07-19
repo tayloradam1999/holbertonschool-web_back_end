1537fb751d6102dd670540f43492e33aaaf6a5ef
Adam Taylor <tayloradam1999@gmail.com>
2021-07-19 22:57:42 +0000

    Implemented usage of Symbol.species

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,7 +6,12 @@ export default class Car {
     this._color = color;
   }
 
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
-    return new Car();
+    const Species = this.constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color);
   }
 }
