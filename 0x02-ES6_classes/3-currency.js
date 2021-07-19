f1ab133114b0f2d8b86625bbff3278848bd55acc
Adam Taylor <tayloradam1999@gmail.com>
2021-07-19 21:19:27 +0000

    Pushing file for task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor (code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // code.getter
+  get code() {
+    return this._code;
+  }
+
+  // code.setter
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  // name.getter
+  get name() {
+    return this._name;
+  }
+
+  // name.setter
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return (`${this._name} (${this._code})`);
+  }
+}
