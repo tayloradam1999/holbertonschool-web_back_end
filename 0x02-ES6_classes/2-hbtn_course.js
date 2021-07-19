8b29473ad1625da037417f41ef9604234e6123b8
Adam Taylor <tayloradam1999@gmail.com>
2021-07-19 20:39:19 +0000

    Pushing file for task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (newStudents instanceof Array) {
+      if (this.newStudents.every((i) => (typeof i === 'string'))) {
+        this._students = newStudents;
+      } else {
+        throw new TypeError('Student name must be a string');
+      }
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
+}
