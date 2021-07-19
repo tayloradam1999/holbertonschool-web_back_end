0ba25d7f9197b43fa752a3388ca1a870e324344b
Adam Taylor <tayloradam1999@gmail.com>
2021-07-19 23:21:28 +0000

    Pushing file for task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/prefer-default-export */
+import Building from './5-building';
+
+export class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(MYFLOORS) {
+    this._floors = MYFLOORS;
+  }
+
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this.floors} floors`);
+  }
+}
