d5db1dee61fc9d86fec94efaa713ab3e312f108a
Adam Taylor <tayloradam1999@gmail.com>
2021-07-21 23:31:15 +0000

    Pushing file for task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,13 @@
 export default function createInt8TypedArray(length, position, value) {
   const myAB = new ArrayBuffer(length);
-  for (let i = 0; i < position; i += 1) {
-    if (i === position) {
-      i = value;
+  const myView = new DataView(myAB, 0);
+  const my8Array = new Int8Array(myAB);
+  for (let x = 0; x < length; x += 1) {
+    if (position > length) {
+      throw new Error('Position outside range');
+    } if (x === position) {
+      my8Array[x] = value;
     }
   }
-  return myAB;
+  return myView;
 }
