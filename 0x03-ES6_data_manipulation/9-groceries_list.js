613005a53f14a41da71b602a37e1b46ba74f84b6
Adam Taylor <tayloradam1999@gmail.com>
2021-07-21 22:52:30 +0000

    Pushing file for task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  const myMap = new Map(([['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]]));
+  return (myMap);
+}
+//   for (let i = 0; i < myList.length[i]; i += 1) {
+//     myMap.set(i[0], i[1]);
+//   }
+//   return myMap;
