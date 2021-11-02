2e79b22736be18623f17cf094ac9f4bfda23e5f2
Adam Taylor <2680@holbertonschool.com>
2021-11-02 17:02:44 -0500

    Fix documentation

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,14 +2,7 @@
 """ Define and annotate variables """
 
 
-# 'a' is an integer
-a = 1 # type: int
-
-# 'pi' is a float
-pi = 3.14 # type: float
-
-# 'i_understand_annotations' is a boolean
-i_understand_annotations = True # type: bool
-
-# 'school' is a string
-school = "Holberton" # type: str
+a: int = 1
+pi : float = 3.14
+i_understand_annotations : bool = True
+school: str = "Holberton"
