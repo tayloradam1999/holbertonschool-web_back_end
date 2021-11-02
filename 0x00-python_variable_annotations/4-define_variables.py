cfb119ef63d64c95df44dd7c1c63b3f1f67d3a6a
Adam Taylor <2680@holbertonschool.com>
2021-11-02 16:59:34 -0500

    Pushing file for task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Define and annotate variables """
+
+# 'a' is an integer
+a = 1
+
+# 'pi' is a float
+pi = 3.14
+
+# 'i_understand_annotations' is a boolean
+i_understand_annotations = True
+
+# 'school' is a string
+school = "Holberton"
