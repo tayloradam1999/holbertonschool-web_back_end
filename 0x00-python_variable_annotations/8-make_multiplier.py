2f0dc4218eb4e39bb4eeaeda7b00b66e0d205acb
Adam Taylor <2680@holbertonschool.com>
2021-11-02 18:32:25 -0500

    Implemented Callable from typing import

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """ Type-annotated function """
 
+from typing import Callable
 
-def make_multiplier(multiplier: float) -> float:
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ Return a function that multiplies its argument by multiplier """
-    def multiply(value: float) -> float:
-        """ Return the product of value and multiplier """
-        return value * multiplier
+    def multiply(number: float) -> float:
+        """ Multiply number by multiplier """
+        return number * multiplier
+
     return multiply
