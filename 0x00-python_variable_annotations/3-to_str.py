7e72608231d8591439da23ccbaf8eb597be10bd8
Adam Taylor <2680@holbertonschool.com>
2021-11-02 16:55:48 -0500

    Pushing file for task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Type-annotated function returns string representation of float """
+
+
+def to_str(n: float) -> str:
+    """ Return string representation of float """
+    return str(n)
