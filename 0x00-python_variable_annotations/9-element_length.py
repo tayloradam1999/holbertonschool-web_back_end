5e23a39a0bb091a974cfff2f09a639df5b9922b0
Adam Taylor <2680@holbertonschool.com>
2021-11-02 18:44:08 -0500

    Pushing file for task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Annotate the below function's parameters and return values
+with the appropriate types """
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Returns element length """
+    return [(i, len(i)) for i in lst]
