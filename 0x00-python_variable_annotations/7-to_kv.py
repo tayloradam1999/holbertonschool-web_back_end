eb38ccfd1be5d835da01609083b93ed8f5a50e2f
Adam Taylor <2680@holbertonschool.com>
2021-11-02 17:32:37 -0500

    Forgot to implement squaring the value!

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,9 +7,9 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Convert key-value pair to tuple
+    Convert key-value pair to tuple where v is squared
     :param k: key
     :param v: value
     :return: tuple
     """
-    return k, v
+    return k, v * v
