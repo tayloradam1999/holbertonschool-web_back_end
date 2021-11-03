16f7d4e646cda661cabf3c933670c24ee23638fe
Adam Taylor <2680@holbertonschool.com>
2021-11-03 15:47:58 -0500

    Changed imports, typing.List instead of List now

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,12 +5,13 @@ The code is nearly identical to wait_n except task_wait_random is being called.
 """
 from typing import List
 import asyncio
+import typing
 
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
     """ wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using sort()
     because of concurrency. """
