4d668e6030b18fa2ff42bad2846bad25841a3af7
Adam Taylor <2680@holbertonschool.com>
2021-11-03 12:57:49 -0500

    Pushing file for task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Import wait_random from the previous python file that you’ve written
+and write an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay. You will spawn wait_random n times
+with the specified max_delay. """
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ wait_n should return the list of all the delays (float values).
+    The list of the delays should be in ascending order without using sort()
+    because of concurrency."""
+    delays: List[float] = []
+
+    for i in range(n):
+        delays.append(await wait_random(max_delay))
+
+    new_list: List[float] = []
+
+    while delays:
+        min = delays[0] # arbitrary number in list
+        for x in delays:
+            if x < min:
+                min = x
+        new_list.append(min)
+        delays.remove(min)
+
+    return new_list
