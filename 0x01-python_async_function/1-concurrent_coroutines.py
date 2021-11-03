f9c18ca8fcf0e32422d8afac0ac27cccb259ec2e
Adam Taylor <2680@holbertonschool.com>
2021-11-03 13:13:37 -0500

    PEP8???????????????????????????

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-""" Import wait_random from the previous python file that you’ve written
+"""
+Import wait_random from the previous python file that you’ve written
 and write an async routine called wait_n that takes in 2 int arguments
 (in this order): n and max_delay. You will spawn wait_random n times
-with the specified max_delay. """
+with the specified max_delay.
+"""
+from typing import List
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
-
-
-from typing import List
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
