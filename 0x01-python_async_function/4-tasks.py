fcc56c2dd646ed91650a151436b68ae63f3ae306
Adam Taylor <2680@holbertonschool.com>
2021-11-03 15:34:08 -0500

    implemented asyncio.as_completed in 2nd for loop condition

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,6 +4,7 @@ Take the code from wait_n and alter it into a new function task_wait_n.
 The code is nearly identical to wait_n except task_wait_random is being called.
 """
 from typing import List
+import asyncio
 
 
 task_wait_random = __import__('3-tasks').task_wait_random
@@ -14,18 +15,9 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     The list of the delays should be in ascending order without using sort()
     because of concurrency. """
     delays: List[float] = []
-
     for i in range(n):
-        delays.append(await task_wait_random(max_delay))
-
-    new_list: List[float] = []
-
-    while delays:
-        min = delays[0]  # arbitrary number in list
-        for x in delays:
-            if x < min:
-                min = x
-        new_list.append(min)
-        delays.remove(min)
-
-    return new_list
+        delays.append(task_wait_random(max_delay))
+    new_delays: List[float] = []
+    for x in asyncio.as_completed(delays):
+        new_delays.append(await x)
+    return new_delays
