08544957ef74a9c9ffc2281dd7e3f409998d0302
Adam Taylor <2680@holbertonschool.com>
2021-11-03 13:17:56 -0500

    PEP8?????????????

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,7 +14,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using sort()
-    because of concurrency."""
+    because of concurrency. """
     delays: List[float] = []
 
     for i in range(n):
