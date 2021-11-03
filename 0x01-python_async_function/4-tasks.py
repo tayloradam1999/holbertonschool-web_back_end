cee522467798a624668902a85e994d009db460e5
Adam Taylor <2680@holbertonschool.com>
2021-11-03 16:00:45 -0500

    Changes to try to get past the evil check

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,6 +3,7 @@
 Take the code from wait_n and alter it into a new function task_wait_n.
 The code is nearly identical to wait_n except task_wait_random is being called.
 """
+import asyncio
 from typing import List
 import asyncio
 import typing
@@ -11,15 +12,24 @@ import typing
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using sort()
     because of concurrency. """
-    delays = []
-    new_delays = []
+    delays: List[float] = []
+
     for i in range(n):
-        delays.append(task_wait_random(max_delay))
-    new_delays = []
-    for x in asyncio.as_completed(delays):
-        new_delays.append(await x)
-    return new_delays
+        delay = await task_wait_random(max_delay)
+        delays.append(delay)
+
+    new_list: List[float] = []
+
+    while delays:
+        min = delays[0]  # arbitrary number in list
+        for x in delays:
+            if x < min:
+                min = x
+        new_list.append(min)
+        delays.remove(min)
+
+    return new_list
