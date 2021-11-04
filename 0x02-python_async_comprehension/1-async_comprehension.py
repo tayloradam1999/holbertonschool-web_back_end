1ee4af0b03e7bb823352d7213f8a8f6034010cdd
Adam Taylor <2680@holbertonschool.com>
2021-11-04 16:58:48 -0500

    Push file for task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Import async_generator from the previous task and
+then write a coroutine called async_comprehension that takes no arguments.
+"""
+import asyncio
+import random
+import typing
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> typing.List[float]:
+    """
+    The coroutine will collect 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers.
+    """
+    result = [i async for i in async_generator()]    
+    return result
+    
