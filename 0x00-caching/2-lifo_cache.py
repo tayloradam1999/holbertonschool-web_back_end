3f2737a48719a1bde38e70f0e44a219bda9c6c4d
Adam Taylor <2680@holbertonschool.com>
2021-11-09 21:30:55 -0600

    Push file for task 2 and edit comment in fifo_cache

--- 0x00-caching/2-lifo_cache.py
+++ 0x00-caching/2-lifo_cache.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """
-Create a class FIFOCache that inherits from BaseCaching
-and is a FIFO caching system
+Create a class LIFOCache that inherits from BaseCaching
+and is a LIFO caching system
 """
 
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """
     -You must use <self.cache_data>: dict from the parent class BaseCaching
 
@@ -28,8 +28,8 @@ class FIFOCache(BaseCaching):
         -If key or item is None, this method should not do anything.
 
         -If the number of items in self.cache_data is higher than
-        BaseCaching.MAX_ITEMS, you must discard the first item
-        put in cache (FIFO algorithm), and you must print DISCARD:
+        BaseCaching.MAX_ITEMS, you must discard the LAST item
+        put in cache (LIFO algorithm), and you must print DISCARD:
         with the key discarded
         """
         if key is None or item is None:
@@ -38,7 +38,7 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discarded_key = list(self.cache_data.keys())[0]
+            discarded_key = list(self.cache_data.keys())[BaseCaching.MAX_ITEMS - 1]
             del self.cache_data[discarded_key]
 
             print("DISCARD: {}".format(discarded_key))
@@ -52,5 +52,5 @@ class FIFOCache(BaseCaching):
         """
         if key is None or key not in self.cache_data:
             return None
-        
+
         return self.cache_data[key]
