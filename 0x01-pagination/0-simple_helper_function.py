29defa95dae1b0fcbcd0c9f612cbadc5c59141d0
Adam Taylor <2680@holbertonschool.com>
2021-11-14 18:06:22 -0600

    Push file for task 0

---
+++ 0x01-pagination/0-simple_helper_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Function <index_range> takes two integer arguments
+<page> and <page_size>
+"""
+from typing import Union, Tuple
+
+
+def index_range(page: int, page_size: int) -> Union[tuple[int, int], None]:
+    """
+    Returns a tuple of size 2 containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+
+    Page numbers are 1 -indexed, the first page is page 1.
+    """
+    if page < 1 or page_size < 1:
+        return None
+    return (page_size * (page - 1), page_size * page)
