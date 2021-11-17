a6094367766b576c290e5f2e65a8eb54698bdc7d
Adam Taylor <tayloradam1999@gmail.com>
2021-11-17 13:31:50 -0600

    Added logger propagate=False line 89

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -86,6 +86,7 @@ def get_logger() -> logging.Logger:
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
+    logger.propagate=False
     handler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(handler)
