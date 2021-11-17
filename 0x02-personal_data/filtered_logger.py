818b2a2a515a9f68e2cb9b1981f7fc78f82cdd6b
Adam Taylor <tayloradam1999@gmail.com>
2021-11-17 12:49:13 -0600

    Push file for tasks 0 and 1

---
+++ 0x02-personal_data/filtered_logger.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Function that returns the log message obfuscated
+"""
+import re
+import logging
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, seperator:
+                 str) -> str:
+    """Obfuscate log msg
+
+    The function should use regex to replace occurences of certain field values
+
+    Function should be < 5 lines and use re.sub() to perform all regex in one
+    statement
+
+    Args:
+        fields (str): list of strs representing all fields to obfuscate
+        redaction (str): str representing what field will be replaced with
+        message (str): str representing the log message
+        seperator (str): str representing the seperator between fields
+
+    Returns:
+        str: str representing the obfuscated log message
+    """
+    log = message.split(seperator)
+
+    for field in fields:
+        for i in range(len(log)): 
+            log[i] = re.sub(field + '=.*', field + '=' + redaction, log[i])
+    return seperator.join(log)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Filters values in incoming log records using <filter_datum>
+
+        Values for fields in <fields> should be filtered.
+
+        DO NOT extrapolate <FORMAT> manually. The <format> method
+        should be less than 5 lines long.
+
+        Args:
+            record (logging.LogRecord): log record to be formatted
+
+        Returns:
+            str: formatted log record
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg,
+                                    self.SEPARATOR)
+        return super().format(record) 
