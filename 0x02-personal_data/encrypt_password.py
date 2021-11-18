89b25872b730f5cf9f264c77ceb6dec11709e4d3
Adam Taylor <tayloradam1999@gmail.com>
2021-11-17 21:06:56 -0600

    Push method for task 6

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
 Use <bcrypt> to perform hasing on password
+
+Use <bcrypt> to validate password
 """
 
 import bcrypt
@@ -11,3 +13,10 @@ def hash_password(password: str) -> bytes:
     Returns salted, hashed byte string password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates password
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
