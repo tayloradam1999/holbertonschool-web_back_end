99cf801236b2439b71235728ae1432d5ea51f5c8
Adam Taylor <tayloradam1999@gmail.com>
2021-11-19 19:37:08 -0600

    Push format for Authentication class, task 3

---
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Manage API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Manage Api Authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Returns:
+            False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns:
+            None
+        
+        <request> will be the Flask request object.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns:
+            None
+
+        <request> will be the Flask request object.
+        """
+        return None
