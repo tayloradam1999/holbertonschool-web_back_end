110259bc0a2b02378b98da6243f2d4a64c97d2fd
Adam Taylor <tayloradam1999@gmail.com>
2021-11-24 13:56:29 -0600

    Add comment for dumb dumb brain

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -40,6 +40,7 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
+        # assigns session_id to key, user_id to value
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
