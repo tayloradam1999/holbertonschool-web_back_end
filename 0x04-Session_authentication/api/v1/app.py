1ade2132014d6e37a3e129d0e863fe7377638cf6
Adam Taylor <tayloradam1999@gmail.com>
2021-11-24 14:20:29 -0600

    Added extra if conditional for abort 401

--- 0x04-Session_authentication/api/v1/app.py
+++ 0x04-Session_authentication/api/v1/app.py
@@ -62,17 +62,21 @@ def auth_before_request() -> str:
     Aborts:
         if auth.authorization_header(request) is None, abort 401
         if auth.current_user(request) is None, abort 403
+        if auth.authorization_header(request) and auth.session_cookie(request)
+            return None, abort 401
     """
     if auth is None:
         return
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+       auth.session_cookie(request) is None):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
