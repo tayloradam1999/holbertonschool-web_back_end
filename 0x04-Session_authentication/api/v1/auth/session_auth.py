d88ac67a69beb143c6c23a616b6b73db01711a45
Adam Taylor <tayloradam1999@gmail.com>
2021-11-23 21:33:02 -0600

    Implement create_session method

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,41 @@
 Session Authentication Module
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
-    Empty for the moment
+    Handles Session Authentication
     """
+    def __init__(self):
+        """
+        Init method
+        """
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session ID for a <user_id>
+
+        Args:
+            user_id (str): The user's ID
+
+        Returns:
+            None if <user_id> is None
+            None if <user_id> is not a string
+            Otherwise:
+                Generates a session ID using the `uuid` module and
+                    uuid4() like <id> in <Base>
+                Uses new session ID as key of the dict <user_id_by_session_id>,
+                    the value for this key must be <user_id>
+                Returns the session ID
+
+        The same <user_id> can have multiple session IDs. The <user_id> is
+            the value in the dictionary <user_id_by_session_id>
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
