61484b99877c5554b2a443e81af592306af55122
Adam Taylor <tayloradam1999@gmail.com>
2021-11-29 15:03:54 -0600

    Push changes for task 5

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Hashes password
 """
 import bcrypt
+from typing import TypeVar
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +13,27 @@ def _hash_password(password: str) -> bytes:
     Returns salted and hashed password using bcrypt.hashpw()
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> TypeVar('User'):
+        """
+        Hashes password with _hash_password, then saves user to database
+        using self._db.add_user() and then returns the User object
+
+
+        If a used already exists with the passed email, raise ValueError
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists.".format(email))
+        except NoResultFound:
+            my_user = self._db.add_user(email, _hash_password(password))
+            return my_user
