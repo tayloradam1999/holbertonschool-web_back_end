1bc11c000b0cca502306801d4d37abcf70583359
Adam Taylor <tayloradam1999@gmail.com>
2021-11-29 16:10:39 -0600

    Push changes for task 8

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -37,3 +37,13 @@ class Auth:
         except NoResultFound:
             my_user = self._db.add_user(email, _hash_password(password))
             return my_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Returns True if user exists and password is correct
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
