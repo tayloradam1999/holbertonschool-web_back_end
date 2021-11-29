05afe10cf866ff3ce39dbac77ec09b9a0d25484a
Adam Taylor <tayloradam1999@gmail.com>
2021-11-29 13:59:48 -0600

    Push file for task 1

---
+++ 0x05-user_authentication_service/db.py
@@ -0,0 +1,45 @@
+"""
+DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from typing import TypeVar
+
+from user import Base
+from user import User
+
+
+class DB:
+    """
+    DB class
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """
+        Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """
+        Returns a User object and saves it to the database
+        """
+        my_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(my_user)
+        self._session.commit()
+        return my_user
