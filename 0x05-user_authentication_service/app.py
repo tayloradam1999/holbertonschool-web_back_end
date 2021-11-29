2d3cee548e7671868deb8813cbeabd4e0cea86c0
Adam Taylor <tayloradam1999@gmail.com>
2021-11-29 15:46:39 -0600

    Push changes for task 7

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -2,7 +2,11 @@
 """
 Creates Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -16,5 +20,24 @@ def index():
     return jsonify({'message': 'Bienvenue'})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+    Expects two form data fields: "email" and "password".
+
+    If the user does not exist, registers it and responds.
+
+    If user already exists, catch exception and return JSON payload and
+        returns 400
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({'email': email, 'message:': 'user created'})
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
