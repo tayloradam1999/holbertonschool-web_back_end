c1f9a3503630c2a67bae02dd7261578adf048dc6
Adam Taylor <tayloradam1999@gmail.com>
2021-11-30 19:51:23 -0600

    Checker

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -32,7 +32,7 @@ def users():
     password = request.form.get('password')
     try:
         AUTH.register_user(email=email, password=password)
-        return jsonify({'email': email, 'message:': 'user created'}), 200
+        return jsonify({'email': email, 'message:': 'user created'})
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
 
