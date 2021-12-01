adf0c5954aa44c54cf48fb11ab6118beb9f23c3f
Adam Taylor <tayloradam1999@gmail.com>
2021-11-30 18:25:03 -0600

    Fix pycodestyle errors

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -30,11 +30,11 @@ class Auth:
 
 
         If a user already exists with the passed email, raise ValueError
-        
+
         Args:
             email (str): user's email
             password (str): user's password
-            
+
         Returns:
             User: User object
         """
@@ -48,16 +48,17 @@ class Auth:
     def valid_login(self, email: str, password: str) -> bool:
         """
         Returns True if user exists and password is correct
-        
+
         Args:
             email (str): user's email
             password (str): user's password
-        
+
         Returns:
             bool: True if user exists and password is correct
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(
+                password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
