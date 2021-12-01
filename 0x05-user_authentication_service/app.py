b08e072430f1f87a55b2773188fb208fe8dba876
Adam Taylor <tayloradam1999@gmail.com>
2021-11-30 18:55:28 -0600

    Add status code 200 return to registering authenticated user

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -34,7 +34,7 @@ def users():
     password = request.form.get('password')
     try:
         AUTH.register_user(email, password)
-        return jsonify({'email': email, 'message:': 'user created'})
+        return jsonify({'email': email, 'message:': 'user created'}), 200
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
 
