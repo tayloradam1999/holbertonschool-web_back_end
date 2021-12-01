61a3ddc2bc435395c3ced77ff39d4730d1914cfd
Adam Taylor <tayloradam1999@gmail.com>
2021-11-30 18:12:42 -0600

    Fix return for add_user()

--- 0x05-user_authentication_service/db.py
+++ 0x05-user_authentication_service/db.py
@@ -38,7 +38,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('user.User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         Returns a User object and saves it to the database
         """
