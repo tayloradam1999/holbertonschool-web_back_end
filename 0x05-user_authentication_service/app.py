5dc5d4b406b8643b8f25705a7586feadf8bfd2d5
Adam Taylor <tayloradam1999@gmail.com>
2021-12-01 20:44:22 -0600

    Checker?

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -69,7 +69,7 @@ def logout():
     sesh_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(sesh_id)
     if user:
-        AUTH.destroy_session(sesh_id)
+        AUTH.destroy_session(user.id)
         return redirect('/', code=302)
     abort(403)
 
