b0e7839e2098566e7e806c7bf182fc83c4cc8659
Adam Taylor <tayloradam1999@gmail.com>
2021-12-01 20:37:48 -0600

    Stored data in variables for readability

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -66,10 +66,11 @@ def logout():
     and redirect the user to 'GET /'. If the user does not exist,
     respond with 403.
     """
-    user = AUTH.get_user_by_session_id(request.cookies.get('session_id'))
+    sesh_id = request.cookies.get('session_id')
+    user = AUTH.get_user_by_session_id(sesh_id)
     if user:
-        AUTH.destroy_session(request.cookies.get('session_id'))
-        return redirect('/', 302)
+        AUTH.destroy_session(sesh_id)
+        return redirect('/', code=302)
     abort(403)
 
 
