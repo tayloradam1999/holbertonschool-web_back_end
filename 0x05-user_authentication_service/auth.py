7fce9a21e9a5cb343038a01f4984f327442a7f92
Adam Taylor <tayloradam1999@gmail.com>
2021-12-01 20:21:36 -0600

    Push task 12

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -89,3 +89,20 @@ class Auth:
         session_id = _generate_uuid()
         user.session_id = session_id
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        Returns corresponding User object from session_id
+
+        Args:
+            session_id (str): UUID
+
+        Returns:
+            If session ID is None, or no user found, return None
+            Otherwise, return User object
+        """
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound as e:
+            return None
+        
