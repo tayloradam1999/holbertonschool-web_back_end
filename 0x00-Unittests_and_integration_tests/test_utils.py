f87a80976b65eadb07a2e671c9c99d1ee1cf9fc1
Adam Taylor <tayloradam1999@gmail.com>
2021-12-06 13:41:28 -0600

    Push file for task 0

---
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Unittests for utils.py
+"""
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+	"""
+	Test access to nested map method	
+	"""
+	@parameterized.expand([
+		{"a": 1}, ("a",),
+		{"a": {"b": 2}}, ("a",),
+		{"a": {"b": 2}}, ("a", "b"),
+	])
+	def test_acces_nested_map(self, d, keys):
+		"""
+		Test access to nested map method
+		"""
+		from utils import access_nested_map
+		self.assertEqual(access_nested_map(d, keys), d[keys[0]][keys[1]])
