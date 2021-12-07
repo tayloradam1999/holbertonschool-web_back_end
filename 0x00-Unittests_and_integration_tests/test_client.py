36e427e124c4402db861771cb9b4c2d8a4dd9586
Adam Taylor <tayloradam1999@gmail.com>
2021-12-07 15:52:04 -0600

    Task 7

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -52,3 +52,15 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(test_return, ["holberton"])
             mock_get_json.assert_called_once
             m.assert_called_once
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Tests has_license method
+        """
+        test_client = GithubOrgClient("holberton")
+        test_return = test_client.has_license(repo, license_key)
+        self.assertEqual(test_return, expected)
