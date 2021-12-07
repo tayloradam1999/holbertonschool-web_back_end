76142fb15e6d9fb6368e8dbf8f897d16692d9c5c
Adam Taylor <tayloradam1999@gmail.com>
2021-12-07 10:56:10 -0600

    Task 6

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -3,9 +3,12 @@
 Tests github org client
 """
 import unittest
-from unittest.mock import patch, PropertyMock, Mock
+from unittest.mock import patch, PropertyMock
+from urllib.error import HTTPError
 from parameterized import parameterized
+from parameterized import parameterized_class
 from client import GithubOrgClient
+from fixtures import *
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -24,17 +27,3 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org)
         client_return = client.org
         self.assertEqual(client_return, mock_get_json.return_value)
-
-    def test_public_repos_url(self):
-        """
-        Tests return value of GithubOrgClient.public_repos_url
-        """
-        with patch.object(GithubOrgClient,
-                          "org",
-                          new_callable=PropertyMock,
-                          return_value={"repos_url": "holberton"}) as mock_org:
-            test_json = {"repos_url": "holberton"}
-            test_client = GithubOrgClient(test_json.get("repos_url"))
-            test_return = test_client._public_repos_url
-            self.assertEqual(
-                test_return, mock_org.return_value.get("repos_url"))
