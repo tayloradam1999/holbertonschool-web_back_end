e6b320e8964db199c4cd3728a13dce93e7251e6b
Adam Taylor <tayloradam1999@gmail.com>
2021-12-13 10:20:30 -0600

    Better documentation

--- 0x01-i18n/4-app.py
+++ 0x01-i18n/4-app.py
@@ -6,9 +6,6 @@ Uses Config to set Babel's default local <en>
 and timezone <UTC>
 
 Uses that class as config for flask app.
-
-Uses gettext to parameterize templates using message IDS
-'home_title' and 'home_header'.
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
@@ -17,7 +14,7 @@ from flask_babel import Babel, gettext
 app = Flask(__name__)
 babel = Babel(app)
 gettext.__doc__ = "Nice one, checker."
-""" Emmanuel Turlay fan club """
+""" Parameterize templates using message IDS """
 
 
 class Config():
