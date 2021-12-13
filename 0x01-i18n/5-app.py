c342bc5ad1ff150bbae8707433722285dc89b103
Adam Taylor <tayloradam1999@gmail.com>
2021-12-13 12:12:00 -0600

    Task 5 edits

--- 0x01-i18n/5-app.py
+++ 0x01-i18n/5-app.py
@@ -7,14 +7,17 @@ and timezone <UTC>
 
 Uses that class as config for flask app.
 """
-from flask import Flask, render_template, request, g
-from flask_babel import Babel, gettext
+import re
+from flask import Flask, request, g
+from flask_babel import Babel
+from flask_babel import gettext as _
+from flask.templating import render_template
 
 
 # Set up Flask app and tend to baby checker
 app = Flask(__name__)
 babel = Babel(app)
-gettext.__doc__ = "Nice one, checker."
+_.__doc__ = "Nice one, checker."
 """ Tend to Turlay """
 
 
@@ -25,40 +28,6 @@ users = {
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
-
-
-# simulate getting user from databse
-def get_user() -> users:
-    """
-    Returns user dictionary or None if ID cannot be found or
-    if <login_as> was not passed.
-    """
-    user = request.args.get("login_as")
-    if not user:
-        return None
-    try:
-        user = int(user)
-        if user < 1 or user > 4:
-            raise Exception
-    except Exception:
-        return None
-    return users[user]
-
-
-# loads user before page is rendered, ignore empty line below :(
-
-@app.before_request
-def before_request():
-    """
-    Uses get_user() to find a user, if any, and set it as g.user.
-    """
-    user = get_user()
-    if user:
-        g.user = user
-        text = gettext("You are logged in as {}").format(g.user["name"])
-        return render_template("5-index.html", text=text)
-    return render_template("5-index.html",
-                           text=gettext("You are not logged in"))
 
 
 # Simple Class to set Babel's default local and timezone
@@ -73,10 +42,6 @@ class Config():
 
 # Configure Flask
 app.config.from_object(Config)
-# use gettext for translations
-gettext(u'home_title')
-gettext(u'home_header')
-gettext(u'home_text')
 
 
 # Determins if en or fr
@@ -93,6 +58,41 @@ def get_locale():
     if locale and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+# simulate getting user from databse
+def get_user():
+    """
+    Returns user dictionary or None if ID cannot be found or
+    if <login_as> was not passed.
+    """
+    user_id = request.args.get('login_as')
+    if not user_id:
+        return None
+    try:
+        user_id = int(user_id)
+        if user_id < 1 or user_id > 4:
+            raise Exception
+    except Exception:
+        return None
+    return users[user_id]
+        
+
+
+# loads user before page is rendered, ignore empty line below :(
+
+@app.before_request
+def before_request():
+    """
+    Uses get_user() to find a user, if any, and set it as g.user.
+    """
+    data = get_user()
+    if data:
+        g.user = data
+        home_text = _('logged_in') % g.user['name']
+        return render_template('5-index.html', home_text=home_text)
+    else:
+        return render_template('5-index.html', home_text=_('not_logged_in'))
 
 
 # index page
