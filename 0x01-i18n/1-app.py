483fc49805fad4d41aee3b74739799e4bc358f35
Adam Taylor <tayloradam1999@gmail.com>
2021-12-12 19:40:49 -0600

    Push files for task 1

--- 0x01-i18n/1-app.py
+++ 0x01-i18n/1-app.py
@@ -27,7 +27,7 @@ def get_locale():
     """
     Get locale from flask.
     """
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    return request.accept_languages.best_match(Config['LANGUAGES'])
 
 
 @app.route('/')
@@ -35,7 +35,7 @@ def index():
     """
     Return the index page.
     """
-    return render_template('index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
