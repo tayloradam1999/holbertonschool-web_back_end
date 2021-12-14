c87faa5ea9b8e20e439d0ef94885f52da220d36a
Adam Taylor <tayloradam1999@gmail.com>
2021-12-14 15:12:48 -0600

    Push updates for task 1

--- 0x00-redis_basic/exercise.py
+++ 0x00-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -23,8 +23,57 @@ class Cache():
         Generates random key with uuid and stores the input
         data in Redis using the key.
 
-        Returns the key.
+        Args:
+            data (Union[str, bytes, int, float]): data to be stored
+
+        Returns:
+            str: key of the stored data
         """
         key = str(uuid.uuid4())
         Cache()._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable[[bytes], str] = None) -> str:
+        """
+        Takes key argument and returns the corresponding data
+        from Redis using the get() method. If the data is not
+        found in Redis, it calls the function fn and stores the
+        returned data in Redis using the key.
+
+        Args:
+            key (str): key of the data to be retrieved
+            fn (Callable[[bytes], str]): function to be called
+                if the data is not found in Redis
+
+        Returns:
+            str: data stored in Redis
+        """
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """
+        Automatically parameterizes <Cache.get> with the correct
+        conversion function
+
+        Args:
+            key (str): key of the data to be retrieved
+
+        Returns:
+            str: data stored in Redis
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """
+        Automatically parameterizes <Cache.get> with the correct
+        conversion function
+
+        Args:
+            key (str): key of the data to be retrieved
+
+        Returns:
+            int: data stored in Redis
+        """
+        return self.get(key, int)
