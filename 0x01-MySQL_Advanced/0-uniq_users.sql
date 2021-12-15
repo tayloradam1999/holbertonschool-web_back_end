66946892db7d4099e4238833bdf8d0cc89b475a0
Adam Taylor <tayloradam1999@gmail.com>
2021-12-15 15:54:57 -0600

    Push new project directory - all req files for task 0

---
+++ 0x01-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creates a table <users>. if it already exists, does not fail.
+-- can be executed on any databse
+CREATE TABLE if NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
---
+++ 0x01-MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+# 0x01-MySQL_Advanced
