610855de48380d3c49336dc48358031f7fc591a4
Adam Taylor <tayloradam1999@gmail.com>
2021-12-15 15:56:24 -0600

    Fix comments

--- 0x01-MySQL_Advanced/0-uniq_users.sql
+++ 0x01-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- creates a table <users>. if it already exists, does not fail.
--- can be executed on any databse
+-- can be executed on any database
 CREATE TABLE if NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
