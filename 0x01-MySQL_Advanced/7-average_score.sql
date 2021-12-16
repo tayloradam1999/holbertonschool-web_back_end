dc9dcd4bf653ff8a0140d1e20c0e78312b7cb375
Adam Taylor <tayloradam1999@gmail.com>
2021-12-16 13:53:57 -0600

    Task 7

---
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- Creates stored procedure <ComputeAverageScoreForUser> that computes and stores the average score for a student.
+-- Note: An average can hold a decimal.
+
+-- ComputeAverageScoreForUser takes 1 parameter:
+-- user_id: a <users.id> value
+DELIMITER |
+CREATE PROCEDURE ComputeAverageScoreForUser (
+	IN user_id INT
+)
+BEGIN
+	DECLARE total FLOAT;
+	DECLARE count INT;
+	DECLARE average FLOAT;
+	SELECT SUM(score) INTO total FROM corrections WHERE user_id = user_id;
+	SELECT COUNT(*) INTO count FROM corrections WHERE user_id = user_id;
+	SET average = total / count;
+	UPDATE users SET average_score = average WHERE id = user_id;
+END |
+
+DELIMITER ;
