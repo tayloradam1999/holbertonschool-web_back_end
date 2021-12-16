8ed177f0691d14461b0f655bc7f5f0e12577fb39
Adam Taylor <tayloradam1999@gmail.com>
2021-12-16 14:36:14 -0600

    Rachel Lewis

--- 0x01-MySQL_Advanced/7-average_score.sql
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -11,8 +11,8 @@ BEGIN
 	DECLARE total FLOAT;
 	DECLARE count INT;
 	DECLARE average FLOAT;
-	SELECT SUM(score) INTO total FROM corrections WHERE user_id = user_id;
-	SELECT COUNT(project_id) INTO count FROM corrections WHERE user_id = user_id;
+	SELECT SUM(score) INTO total FROM corrections WHERE corrections.user_id = user_id;
+	SELECT COUNT(project_id) INTO count FROM corrections WHERE corrections.user_id = user_id;
 	SET average = total / count;
 	UPDATE users SET average_score = average WHERE id = user_id;
 END |
