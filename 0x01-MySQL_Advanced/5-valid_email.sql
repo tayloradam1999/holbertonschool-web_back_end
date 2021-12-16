38295256c16b62314fa59942a4dd92a1d161c897
Adam Taylor <tayloradam1999@gmail.com>
2021-12-16 12:55:55 -0600

    Implement Delimiter + Begin/End for calculations

--- 0x01-MySQL_Advanced/5-valid_email.sql
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,11 @@
 -- Creates a trigger that resets the attribute <valid_email> when then <email> has been changed.
-CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users.email
-FOR EACH ROW UPDATE users
-SET valid_email = 0
-WHERE email != NEW.email;
+DELIMITER |
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+  IF NEW.email <> OLD.email THEN
+	SET NEW.valid_email = 0;
+  END IF;
+END |
+
+DELIMITER ;
