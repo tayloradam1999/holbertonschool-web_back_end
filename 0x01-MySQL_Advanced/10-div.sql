9ba865cdb52b0954e4a10513f90a48ce5eae053e
Adam Taylor <tayloradam1999@gmail.com>
2021-12-16 13:26:43 -0600

    Task 10

---
+++ 0x01-MySQL_Advanced/10-div.sql
@@ -0,0 +1,20 @@
+-- Function that divdes and returns the first number by the second number or 0 if the second number is 0
+-- @param a: first number
+-- @param b: second number
+-- @return: the first number divided by the second number or 0 if the second number is 0
+DELIMITER |
+CREATE FUNCTION SafeDiv (
+	a INT,
+	b INT
+)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	ELSE
+		RETURN a DIV b;
+	END IF;
+END |
+
+DELIMITER ;
