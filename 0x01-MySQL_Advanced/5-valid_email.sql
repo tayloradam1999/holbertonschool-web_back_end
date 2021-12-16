5e93f8e7efedf2c8c593662c332343f7a21e9e62
Adam Taylor <tayloradam1999@gmail.com>
2021-12-16 12:43:35 -0600

    Change AFTER UPDATE ON to users.email

--- 0x01-MySQL_Advanced/5-valid_email.sql
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,5 @@
 -- Creates a trigger that resets the attribute <valid_email> when then <email> has been changed.
-CREATE TRIGGER reset_valid_email AFTER UPDATE ON users
+CREATE TRIGGER reset_valid_email AFTER UPDATE ON users.email
 FOR EACH ROW UPDATE users
 SET valid_email = 0
 WHERE email != NEW.email;
