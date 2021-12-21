c126d7822434318601643d27e499f26f1bb2d48f
Adam Taylor <tayloradam1999@gmail.com>
2021-12-21 14:25:33 -0600

    Task 8

---
+++ 0x02-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Lists all documents in a mongodb collection
+"""
+
+
+def list_all(mongo_collection):
+	"""
+	Args:
+		mongo_collection: A pymongo.collection object
+	
+	Returns:
+		All documents in collection
+		Empty list if no documents
+	"""
+	return mongo_collection.find()
