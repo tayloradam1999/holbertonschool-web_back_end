08092fea7959bb644ee223276d5a63c67efc94fb
Adam Taylor <tayloradam1999@gmail.com>
2021-12-21 14:29:27 -0600

    Task 10

---
+++ 0x02-NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Changes all topics of a school document based on the name of the school
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+	"""
+	Args:
+		mongo_collection: A pymongo.collection object
+		name: The name of the school to update
+		topics: A list of topics to update
+	
+	Returns:
+		- The document updated
+		- None if no document updated
+	"""
+	return mongo_collection.update_one({'name': name}, {'$set': {'topics': topics}}).modified_count
