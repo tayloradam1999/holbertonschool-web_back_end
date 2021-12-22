46dbf3412769d05bb1471397ff34b6be4a6d19c5
Adam Taylor <tayloradam1999@gmail.com>
2021-12-21 19:05:41 -0600

    Passing tests locally but not via the checker

--- 0x02-NoSQL/12-log_stats.py
+++ 0x02-NoSQL/12-log_stats.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python3
 """
 Provides some stats about Nginx logs stored in MongoDB
-
-Database: logs
-Collection: nginx
-Display:
-	- First line: x logs where x is the number of documents in the collection
-	- second line: "Methods:"
-	- 5 lines with the number of documents with the <method> =
-	["GET", "POST", "PUT", "PATCH", "DELETE"] in this order
-	- one line with the number of documents with:
-		- methods=GET
-		- path=/status
 """
-
-
 import pymongo
 
 
 def log():
     """
-    Main function
+    Handles printing the stats
     """
     client = pymongo.MongoClient()
     db = client.logs
