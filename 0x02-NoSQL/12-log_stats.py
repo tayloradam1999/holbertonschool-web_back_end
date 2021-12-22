0ee0edf8971d2a5834d94536185a349f84526400
Adam Taylor <tayloradam1999@gmail.com>
2021-12-21 19:01:32 -0600

    Address pycodestyle

--- 0x02-NoSQL/12-log_stats.py
+++ 0x02-NoSQL/12-log_stats.py
@@ -19,22 +19,25 @@ import pymongo
 
 
 def log():
-	"""
-	Main function
-	"""
-	client = pymongo.MongoClient()
-	db = client.logs
-	collection = db.nginx
+    """
+    Main function
+    """
+    client = pymongo.MongoClient()
+    db = client.logs
+    collection = db.nginx
 
-	print("{} logs".format(collection.count_documents({})))
+    print("{} logs".format(collection.count_documents({})))
 
-	print("Methods:")
-	methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print("Methods:")
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
-	for method in methods:
-		print("\tmethod {}: {}".format(method, collection.count_documents({"method": method})))
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method, collection.count_documents({"method": method})))
 
-	print("{} status check".format(collection.count_documents({"method": "GET", "path": "/status"})))
+    print("{} status check".format(collection.count_documents(
+        {"method": "GET", "path": "/status"})))
+
 
 if __name__ == "__main__":
     log()
