5b60bfcf8cc5abd685b199fadfeb885ddc3e81dd
Adam Taylor <tayloradam1999@gmail.com>
2022-01-05 22:39:10 +0000

    What a mess of a task LOL

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -18,20 +18,20 @@ const countStudents = async (path) => {
   // start looping for students in each field
   // declare dict to populate with num of students in field + list of students
   const dict = {};
-  for (let i = 0; i < uniqueFields.length; i++) {
-	// get number of students in each field
-	const studentsInField = fileData.split('\n').filter((line) => line.includes(uniqueFields[i])).length;
-	// get list of students in each field
-	const studentsInFieldList = fileData.split('\n').filter((line) => line.includes(uniqueFields[i])).map((line) => line.split(',')[0]);
-	// add spaces between elements in arrays
-	const studentsInFieldListString = studentsInFieldList.join(', ');
-	// log
-	console.log(`Number of students in ${uniqueFields[i]}: ${studentsInField}. List: ${studentsInFieldListString}`);
-	// add to dict
-	dict[uniqueFields[i]] = {
-		num: studentsInField,
-		list: studentsInFieldListString,
-	};
+  for (let i = 0; i < uniqueFields.length; i += 1) {
+    // get number of students in each field
+    const studentsInField = fileData.split('\n').filter((line) => line.includes(uniqueFields[i])).length;
+    // get list of students in each field
+    const studentsInFieldList = fileData.split('\n').filter((line) => line.includes(uniqueFields[i])).map((line) => line.split(',')[0]);
+    // add spaces between elements in arrays
+    const studentsInFieldListString = studentsInFieldList.join(', ');
+    // log
+    console.log(`Number of students in ${uniqueFields[i]}: ${studentsInField}. List: ${studentsInFieldListString}`);
+    // add to dict
+    dict[uniqueFields[i]] = {
+      num: studentsInField,
+      list: studentsInFieldListString,
+    };
   }
   return dict;
 };
