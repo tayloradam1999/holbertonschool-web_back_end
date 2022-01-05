a86c85365dc6fe2643dbe8dec2397e801e21bbe0
Adam Taylor <tayloradam1999@gmail.com>
2022-01-05 20:43:57 +0000

    Catch if name is null for checker

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -5,7 +5,10 @@ process.stdin
   .on('readable', () => {
     const name = process.stdin.read();
     // after collecting name from stdin, prints it
-    process.stdout.write(`Your name is: ${name}`);
+    // null check for checker
+    if (name !== null) {
+      process.stdout.write(`Your name is: ${name}`);
+    }
   })
   // when user exits program, log last statement
 
