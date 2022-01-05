efbc7e9cfa52bac6ddd42698ff38e163a81eef83
Adam Taylor <tayloradam1999@gmail.com>
2022-01-05 20:29:30 +0000

    Attached 2nd .on to process.stdin instead of being seperated'

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
+// Log first line
 console.log('Welcome to Holberton School, what is your name?');
-// User should input name on a new line
-process.stdin.on('data', data => {
-  // Gather user name from stdin
-  const name = data.toString();
-  // Log users name
-  console.log('Your name is: %s', name);
-  process.exit();
-});
-process.on('exit', data2 => {
-  console.log('This important software is now closing');
-});
+// Start stdin process
+process.stdin
+  .on('data', (data) => {
+    const name = data.toString().trim();
+    // after collecting name from stdin, prints it
+    console.log('Your name is: %s', name);
+  })
+  // when user exits program, log last statement
+  .on('end', () => {
+    console.log('This important software is now closing');
+  });
+// End of program!
