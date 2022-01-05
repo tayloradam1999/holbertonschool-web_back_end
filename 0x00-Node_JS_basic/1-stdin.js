8f85cbcab4efb026eab7f115c21c2d382ac007ce
Adam Taylor <tayloradam1999@gmail.com>
2022-01-05 20:39:56 +0000

    quote on quote 'better' documentation

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -2,13 +2,16 @@
 console.log('Welcome to Holberton School, what is your name?');
 // Start stdin process
 process.stdin
-  .on('data', (data) => {
-    const name = data.toString().trim();
+  .on('readable', () => {
+    const name = process.stdin.read();
     // after collecting name from stdin, prints it
-    console.log('Your name is: %s', name);
+    process.stdout.write(`Your name is: ${name}`);
   })
   // when user exits program, log last statement
+
+  // this is where the child process is taken into account.
+  // the difference lies in when the program will end.
   .on('end', () => {
-    console.log('This important software is now closing');
+    process.stdout.write('This important software is now closing\n');
   });
 // End of program!
