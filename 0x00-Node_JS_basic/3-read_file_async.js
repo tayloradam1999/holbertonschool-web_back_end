7ef4f069d263631eb61cffc26ca93085332dc643
Adam Taylor <tayloradam1999@gmail.com>
2022-01-05 16:14:18 -0600

    Push for sandbox

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -8,21 +8,23 @@ function countStudents(path) {
 	} catch (err) {
 		throw new Error('Cannot load the database');
 	}
-	return fileData.then((data) => {
-		// Calculate number of students total in database
-		// DOES NOT INCLUDE HEADER LINE OF CSV FILE AND BLANK LINES
-		console.log(`Number of students: ${data.split('\n').length - 2}`);
-		// calculate CS fields and SWE fields
-		const CS = data.split('\n').filter((line) => line.includes('CS')).length;
-		const SWE = data.split('\n').filter((line) => line.includes('SWE')).length;
-		// Put all student names of CS and SWE fields in arrays
-		let CSstudents = data.split('\n').filter((line) => line.includes('CS')).map((line) => line.split(',')[0]);
-		let SWEstudents = data.split('\n').filter((line) => line.includes('SWE')).map((line) => line.split(',')[0]);
-		// add spaces between elements in arrays
-		CSstudents = CSstudents.join(', ');
-		SWEstudents = SWEstudents.join(', ');
-		// log
-		console.log(`Number of students in CS: ${CS}. List: ${CSstudents}`);
-		console.log(`Number of students in SWE: ${SWE}. List: ${SWEstudents}`);
-	});
+	return fileData(path);
 }
+
+countStudents().then(data => {
+	// Calculate number of students total in database
+	// DOES NOT INCLUDE HEADER LINE OF CSV FILE AND BLANK LINES
+	console.log(`Number of students: ${data.split('\n').length - 2}`);
+	// calculate CS fields and SWE fields
+	const CS = data.split('\n').filter((line) => line.includes('CS')).length;
+	const SWE = data.split('\n').filter((line) => line.includes('SWE')).length;
+	// Put all student names of CS and SWE fields in arrays
+	let CSstudents = data.split('\n').filter((line) => line.includes('CS')).map((line) => line.split(',')[0]);
+	let SWEstudents = data.split('\n').filter((line) => line.includes('SWE')).map((line) => line.split(',')[0]);
+	// add spaces between elements in arrays
+	CSstudents = CSstudents.join(', ');
+	SWEstudents = SWEstudents.join(', ');
+	// log
+	console.log(`Number of students in CS: ${CS}. List: ${CSstudents}`);
+	console.log(`Number of students in SWE: ${SWE}. List: ${SWEstudents}`);
+})
