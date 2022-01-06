cf1d2f778e71d6e4bfbfef8e382e132dae31580e
Adam Taylor <tayloradam1999@gmail.com>
2022-01-06 18:31:18 +0000

    Unsure on how to print promise object

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -4,20 +4,18 @@
 // same content as the file 3-read_file_async.js
 // Name of the database passed as arugment to file
 
-const countStudents = require('./3-read_file_async');
 const http = require('http');
-
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	if (req.url === '/') {
-		res.end('Hello Holberton School!');
-	}
-	else if (req.url === '/students') {
-		res.end('This is the list of students' <br> + countStudents('./database.csv'));
-	}
-	else {
-		throw new Error('Not a valid URL');
-	}
-  }).listen(1245);
-  
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.end('This is the list of students\n' + countStudents('./database.csv'));
+  } else {
+    throw new Error('Not a valid URL');
+  }
+}).listen(1245);
+
+module.exports = app;
