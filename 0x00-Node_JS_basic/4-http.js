3d1600cdfcfde008c35e9a97b7c196ca18533b11
Adam Taylor <tayloradam1999@gmail.com>
2022-01-06 18:08:14 +0000

    Remove 2nd function, export first

--- 0x00-Node_JS_basic/4-http.js
+++ 0x00-Node_JS_basic/4-http.js
@@ -3,17 +3,11 @@
 // listens on port 1245
 // displays text for any endpoint
 
-const app = require('http');
+const http = require('http');
 
-const host = 'localhost';
-const port = 1245;
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('Hello Holberton School!');
+}).listen(1245);
 
-const requestListener = function write(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Helllo Holberton School!\n');
-};
-
-const server = app.createServer(requestListener);
-server.listen(port, host, () => {
-  console.log('Server running');
-});
+module.exports = app;
