7a54cd85b3cbb97bec5db74afa05b8091104e79e
Adam Taylor <tayloradam1999@gmail.com>
2022-01-06 18:37:38 +0000

    Push 6 for checker

--- 0x00-Node_JS_basic/6-http_express.js
+++ 0x00-Node_JS_basic/6-http_express.js
@@ -2,11 +2,16 @@
 // same as 4-http_server.js but only responds to '/'
 
 const express = require('express');
-const app = express();
 
+const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
-	res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log('Server is running');
 });
 
 module.exports = app;
