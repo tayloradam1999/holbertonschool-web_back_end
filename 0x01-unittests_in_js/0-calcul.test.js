99a31c8055f958c4e9f28687d58959364d6a258a
Adam Taylor <tayloradam1999@gmail.com>
2022-01-11 15:08:36 -0600

    Separate tests into individual suites

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -7,16 +7,16 @@ describe ("test suite for first number", function() {
 	it("Tests if first number is rounded", function () {
 		assert.equal(calculateNumber(4.5,5), 10);
 	});
-});
+})
 
 describe ("test suite for second number", function() {
 	it("Tests if second number is rounded", function () {
 		assert.equal(calculateNumber(5,4.5), 10);
 	});
-});
+})
 
 describe ("test suite for both numbers", function() {
 	it("Tests if both numbers are rounded", function () {
 		assert.equal(calculateNumber(4.5,4.5), 10);
 	});
-});
+})
