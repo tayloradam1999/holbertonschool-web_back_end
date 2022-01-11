b4b458638fe5487407b5cfa695fcf43c849e5a6e
Adam Taylor <tayloradam1999@gmail.com>
2022-01-11 15:14:23 -0600

    Code refactor

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,18 @@
-var calculateNumber = require('./0-calcul')
-var chai = require('chai');
-var assert = require('assert');
+const calculateNumber = require('./0-calcul')
+const assert = require('assert');
 
 
-describe ("test suite for first number", function() {
-	it("Tests if first number is rounded", function () {
-		assert.equal(calculateNumber(4.5,5), 10);
-	});
-})
-
-describe ("test suite for second number", function() {
-	it("Tests if second number is rounded", function () {
-		assert.equal(calculateNumber(5,4.5), 10);
-	});
-})
-
-describe ("test suite for both numbers", function() {
-	it("Tests if both numbers are rounded", function () {
-		assert.equal(calculateNumber(4.5,4.5), 10);
+describe ("Test suite", function() {
+	it("Tests that a and b are rounded properly using Math.round()", function() {
+		assert.equal(calculateNumber(1.1, 2.2), 3);
+		assert.equal(calculateNumber(1.4, 1,4), 2);
+		assert.equal(calculateNumber(1.5, 1.5), 4);
+		assert.equal(calculateNumber(1.6, 1.6), 4);
+		assert.equal(calculateNumber(1.7, 1), 3);
+		assert.equal(calculateNumber(1, 1.8), 3);
+		assert.equal(calculateNumber(-5, 0.5), -4);
+		assert.equal(calculateNumber(0, -10.98), -11);
+		assert.equal(calculateNumber(1.1, 1.9), 3);
+		assert.equal(calculateNumber(1.1, 0), 1);
 	});
 })
