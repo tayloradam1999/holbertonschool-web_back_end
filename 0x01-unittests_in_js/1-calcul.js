989aec0bf4db0eeea641823bde61e24a6af2fdf3
Adam Taylor <tayloradam1999@gmail.com>
2022-01-11 15:28:17 -0600

    Remove multiplication tests and add Error for div

--- 0x01-unittests_in_js/1-calcul.js
+++ 0x01-unittests_in_js/1-calcul.js
@@ -1,14 +1,19 @@
 // function to round a and b and return sum
 
 const calculateNumber = (type, a, b) => {
-	if (type === '+') {
+	if (type === 'SUM') {
 		return Math.round(a) + Math.round(b);
-	} else if (type === '-') {
+	}
+	if (type === 'SUBTRACT') {
 		return Math.round(a) - Math.round(b);
-	} else if (type === '*') {
-		return Math.round(a) * Math.round(b);
-	} else if (type === '/') {
-		return Math.round(a) / Math.round(b);
+	}
+	if (type === 'DIVIDE') {
+		a = Math.round(a);
+		b = Math.round(b);
+		if (b === 0) {
+			return 'Error';
+		}
+		return a / b;
 	}
 }
 
