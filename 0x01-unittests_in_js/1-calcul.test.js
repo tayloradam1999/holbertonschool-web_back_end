a95f4121caed0ce45d7d9d3867be4634c85b2cf3
Adam Taylor <tayloradam1999@gmail.com>
2022-01-11 15:30:21 -0600

    Checker problems

--- 0x01-unittests_in_js/1-calcul.test.js
+++ 0x01-unittests_in_js/1-calcul.test.js
@@ -4,17 +4,16 @@ const assert = require('assert');
 
 describe ("Test suite", function() {
 	it("Tests that a and b are rounded properly using Math.round()", function() {
-		assert.equal(calculateNumber('+', 1.1, 1.2), 2);
-		assert.equal(calculateNumber('+', 9.9, 0), 10);
-		assert.equal(calculateNumber('+', 1, 1.6), 3);
-		assert.equal(calculateNumber('-', 1.1, 1.2), 0);
-		assert.equal(calculateNumber('-', 9.9, 5), 5);
-		assert.equal(calculateNumber('-', 1, -1.1), 2);
-		assert.equal(calculateNumber('*', 1.1, 1.2), 1);
-		assert.equal(calculateNumber('*', 9.9, 5), 50);
-		assert.equal(calculateNumber('*', 1, -1.1), -1);
-		assert.equal(calculateNumber('/', 1.1, 1.2), 1);
-		assert.equal(calculateNumber('/', 9.9, 5), 2);
-		assert.equal(calculateNumber('/', 1, 1.1), 1);
+		assert.equal(calculateNumber('SUM', 1.1, 1.2), 2);
+		assert.equal(calculateNumber('SUM', 9.9, 0), 10);
+		assert.equal(calculateNumber('SUM', 1, 1.6), 3);
+		assert.equal(calculateNumber('SUBTRACT', 1.1, 1.2), 0);
+		assert.equal(calculateNumber('SUBTRACT', 9.9, 5), 5);
+		assert.equal(calculateNumber('SUBTRACT', 1, -1.1), 2);
+		assert.equal(calculateNumber('DIVIDE', 1.1, 1.2), 1);
+		assert.equal(calculateNumber('DIVIDE', 9.9, 5), 2);
+		assert.equal(calculateNumber('DIVIDE', 1, 1.1), 1);
+		assert.equal(calculateNumber('DIVIDE', 1, 0), 'Error');
+		assert.equal(calculateNumber('DIVIDE', 1, 0.1), 'Error');
 	});
 })
