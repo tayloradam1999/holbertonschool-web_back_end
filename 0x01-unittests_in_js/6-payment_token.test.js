5fbfb9ead7504f8860df5dbaa65517e99204eaf5
Adam Taylor <tayloradam1999@gmail.com>
2022-01-11 19:49:10 -0600

    Fix chai expect in .then

--- 0x01-unittests_in_js/6-payment_token.test.js
+++ 0x01-unittests_in_js/6-payment_token.test.js
@@ -6,7 +6,7 @@ const chai = require('chai');
 describe('Test suite', function () {
 	it('Tests that getPaymentTokenFromApi returns a new Promise', function (done) {
 		getPaymmentTokenFromApi(true).then(function (result) {
-			chai.expect(result).to.be.an('object');
+			chai.expect(result.data).to.equal('Successful response from the API');
 			done();
 		});
 	});
