1c72b144eb98447ca17598af18d68c8af0e28a98
Adam Taylor <tayloradam1999@gmail.com>
2022-01-12 15:57:58 -0600

    Task 9

---
+++ 0x01-unittests_in_js/9-api/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// create instance of express called app
+const app = express();
+// listen to specific port and logs to browser console when express
+// server is started
+const port = 7865;
+app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
+});
+// create route 'GET /'
+app.get('/', (req, res) => {
+	res.send(`Welcome to the payment system`);
+});
+// create route 'GET /cart/:id'
+// :id must only be a number (validation in route definition)
+app.get('/cart/:id', (req, res) => {
+	res.send(`Payment methods for cart ${req.params.id}`);
+});
+// export app
+module.exports = app;
---
+++ 0x01-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,42 @@
+// Unittest file for api.js
+const { expect } = require('chai');
+const mocha = require('mocha');
+const request = require('request');
+
+
+// Test suite for index page
+describe('Test suite for index page for api.js', () => {
+	// Correct status code for index page
+	it ('Checks for correct HTTP status code', (done) => {
+		request('http://localhost:7865', (error, response, body) => {
+			expect(response.statusCode).to.equal(200);
+			done();
+		});
+	});
+	// Correct result for index page 'Welcome to the payment system'
+	it ('Checks for correct result for index page', (done) => {
+		request('http://localhost:7865', (error, response, body) => {
+			expect(body).to.equal('Welcome to the payment system');
+			done();
+		});
+	});
+})
+
+
+// Test suite for cart page
+describe('Test suite for cart page for api.js', () => {
+	// Correct status code when :id is a number
+	it ('Checks for correct HTTP status code when :id is a number', (done) => {
+		request('http://localhost:7865/cart/1', (error, response, body) => {
+			expect(response.statusCode).to.equal(200);
+			done();
+		});
+	});
+	// Correct status code when :id is not a number
+	it ('Checks for correct HTTP status code when :id is not a number', (done) => {
+		request('http://localhost:7865/cart/a', (error, response, body) => {
+			expect(response.statusCode).to.equal(400);
+			done();
+		});
+	});
+})
