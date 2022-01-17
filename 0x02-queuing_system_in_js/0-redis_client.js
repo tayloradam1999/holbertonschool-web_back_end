7e1c745e05395620056bc1b1e45e8168381208a4
Adam Taylor <tayloradam1999@gmail.com>
2022-01-17 14:01:26 -0600

    Edits for task 1

--- 0x02-queuing_system_in_js/0-redis_client.js
+++ 0x02-queuing_system_in_js/0-redis_client.js
@@ -1,1 +1,12 @@
-// Connects to Redis server and logs when connected or connection error
+// Using Babel, connects to Redis server
+
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', () => {
+	console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+	console.log('Redis client not connected to then server: ' + err);
+});
