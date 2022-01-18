0881e3e183d1495e11007191314c90baaa3bc088
Adam Taylor <tayloradam1999@gmail.com>
2022-01-18 15:12:21 -0600

    Task 8

---
+++ 0x02-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,86 @@
+// Create array of jobs
+const jobs = [
+	{
+	  phoneNumber: '4153518780',
+	  message: 'This is the code 1234 to verify your account'
+	},
+	{
+	  phoneNumber: '4153518781',
+	  message: 'This is the code 4562 to verify your account'
+	},
+	{
+	  phoneNumber: '4153518743',
+	  message: 'This is the code 4321 to verify your account'
+	},
+	{
+	  phoneNumber: '4153538781',
+	  message: 'This is the code 4562 to verify your account'
+	},
+	{
+	  phoneNumber: '4153118782',
+	  message: 'This is the code 4321 to verify your account'
+	},
+	{
+	  phoneNumber: '4153718781',
+	  message: 'This is the code 4562 to verify your account'
+	},
+	{
+	  phoneNumber: '4159518782',
+	  message: 'This is the code 4321 to verify your account'
+	},
+	{
+	  phoneNumber: '4158718781',
+	  message: 'This is the code 4562 to verify your account'
+	},
+	{
+	  phoneNumber: '4153818782',
+	  message: 'This is the code 4321 to verify your account'
+	},
+	{
+	  phoneNumber: '4154318781',
+	  message: 'This is the code 4562 to verify your account'
+	},
+	{
+	  phoneNumber: '4151218782',
+	  message: 'This is the code 4321 to verify your account'
+	}
+  ];
+
+// Create Kue queue
+const kue = require('kue');
+const queue = kue.createQueue();
+
+// Iterate through each object in jobs array
+for (let i = 0; i < jobs.length; i++) {
+  	// create job
+	const job = queue.create('push_notification_code_2', jobs[i]).save(
+		function(err) {
+			if (err) {
+				console.log(`Notification job $${job.id} failed: ${err}`);
+			}
+			console.log(`Notification job created: ${job.id}`);
+		},
+		console.log(`Notification job completed`),
+	// track progress
+	).on('progress', (progress) => {
+		console.log(`Notification job ${job.id} ${progress}% complete`);
+	});
+}
+
+// .forEach approach threw err: 'identifier 'job' has already been declared'
+
+// jobs.forEach(job2 => {
+  	// create job
+// 	const job = queue.create('push_notification_code_2', job2).save(
+// 		function(err) {
+// 			if (err) {
+// 				console.log(`Notification job $${job2.id} failed: ${err}`);
+// 			}
+// 			console.log(`Notification job created: ${job2.id}`);
+// 		},
+// 	console.log(`Notification job ${job2.id} completed`),)
+  	// track progress
+// 	.on('progress', (progress) => {
+// 		console.log(`Notification job ${job2.id} ${progress}% complete`);
+// 	});
+// });
