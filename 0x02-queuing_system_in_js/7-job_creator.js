6f702503a356444cbda75203f9637c4449bc461c
Adam Taylor <tayloradam1999@gmail.com>
2022-01-19 19:26:50 -0600

    Fix documentation

--- 0x02-queuing_system_in_js/7-job_creator.js
+++ 0x02-queuing_system_in_js/7-job_creator.js
@@ -53,22 +53,22 @@ const queueKey = 'push_notification_code_2';
 
 // Iterate through each object in jobObjs array
 jobs.forEach((jobObj) => {
-	// create jobObj
+	// create job
 	const job = queue.create(queueKey, jobObj).save((err) => {
 		if (!err) console.log(`Notification job created: ${job.id}`);
 	});
 
-	// listen for jobObj completion
+	// listen for job completion
 	job.on('complete', (result) => {
 		console.log(`Notification job ${job.id} completed`);
 	});
 
-	// listen for jobObj failure
+	// listen for job failure
 	job.on('failed', (errorMessage) => {
 		console.log(`Notification job ${job.id} failed: ${errorMessage}`);
 	});
 
-	// listen for jobObj progress
+	// listen for job progress
 	job.on('progress', (progress) => {
 		console.log(`Notification job ${job.id} ${progress}% complete`);
 	});
